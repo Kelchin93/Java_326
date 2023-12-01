d2752b76828f60acd9b20530b5216d2a82cb622e
Kego <Kelchin93e@icloud.com>
2023-12-01 19:53:29 +0500

    Exceptions

--- 06-OOP/Exceptions/src/Main.java
+++ 06-OOP/Exceptions/src/Main.java
@@ -1,8 +1,11 @@
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello world!");
+        /*System.out.println("Hello world!");
         try {
             factorial(13);
             load(" ");
@@ -15,10 +18,36 @@ public class Main {
         {
             System.out.println(e.getMessage());
         }
-        System.out.println("The end");
+        System.out.println("The end");*/
+       /* try
+        {
+            FileWriter fileWriter = new FileWriter("File.txt");
+            fileWriter.write("IO Exception check");
+            fileWriter.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }*/
+        try {
+            FileReader fileReader = new FileReader("C:\\File.txt");
+            char[] buffer = new char[100];
+            fileReader.read(buffer);
+            System.out.println(buffer);
+            fileReader.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        /*catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }*/
     }
 
-    static int factorial(int n) throws RuntimeException {
+    static int factorial(int n) throws RuntimeException, FileNotFoundException
+    {
         if (n > 12) throw new RuntimeException("Test Exception");
         return 0;
     }
@@ -27,5 +56,5 @@ public class Main {
         if (filename == null) throw new FileNotFoundException("File not Found");
         return "OK";
     }
-    catch(IndexOutOfBoundsException)
+    catch (IndexOutOfBoundsException)
 }
